3ec7e3e3d22a6611ff17aab813871de3fce41d9a
thevinter <theniki98@gmail.com>
2021-06-20 13:53:43 +0200

    updated stuff

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 killall -q polybar
 echo "---" | tee -a /tmp/polybar1.log 
-polybar example >> /tmp/polybar1.log 2>&1 &
+polybar bottom >> /tmp/polybar1.log 2>&1 &
 echo "Launched"
 
